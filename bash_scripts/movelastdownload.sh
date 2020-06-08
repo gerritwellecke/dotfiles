254c9e299c2c2c76d6c8924a7b6201ca36b9147d
wavecorner <48245861+wavecorner@users.noreply.github.com>
2020-06-08 13:54:23 +0200

    Fixed problem moving multiple files

--- bash_scripts/movelastdownload.sh
+++ bash_scripts/movelastdownload.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-ERRUSE="Usage: $0 TARGET-DIRECTORY"
+ERRUSE="Usage: $0 TARGET-DIRECTORY [NUMBER-OF-FILES]"
 ERRDIR="TARGET-DIRECTORY not found"
 
 TARGET=$1
+NUM=${2:-1}
 
-if [[ "$#" != "1" ]]; then
+if (( $# < 1 )); then
     echo $ERRUSE
     exit 1
 elif [[ ! -d $TARGET ]]; then
     echo $ERRDIR
     exit 2
 else
-    LAST_DOWNLOAD=`ls -t ~/Downloads | head -n 1`
-    mv ~/Downloads/$LAST_DOWNLOAD $TARGET
+    LAST_DOWNLOAD=`ls -t ~/Downloads | head -n $NUM`
+    for f in $LAST_DOWNLOAD; do
+        mv ~/Downloads/$f $TARGET
+    done
     exit $?
 fi
